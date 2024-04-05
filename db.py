14e9007a9c74f7932947597f4f9ca02607a371c7
Zedii <jzedlf@gmail.com>
2024-04-06 00:26:42 +0300

    remove testing stuff

--- db.py
+++ db.py
@@ -15,21 +15,7 @@ def create_tables_if_missing():
 			schema_sql = file.read()
 			print("Creating tables...")
 			db.session.execute(text(schema_sql))
-
-			#generate random users for testing
-			for i in range(50):
-				un = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-				pw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-				query = text("INSERT INTO users (username, password) VALUES (:username, :password)")
-				db.session.execute(query, {"username" : un, "password" : pw})
-
-				#random admin state
-				if random.random() < 0.05:
-					db.session.execute(text("INSERT INTO admins (user_id) VALUES (:num)"), {"num":i+3})
-
 			db.session.commit()
-
-		
 
 
 def drop_all_tables():
