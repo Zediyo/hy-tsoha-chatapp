a435dcf3a4d18390ef7cdaffe769bcdd182e49b2
Zedii <jzedlf@gmail.com>
2024-04-05 17:04:38 +0300

    queries for profile page

--- queries.py
+++ queries.py
@@ -56,15 +56,30 @@ def send_friend_request(id1, id2):
 	return True
 
 def get_sent_friend_requests(id):
-	query = text("SELECT receiver_id FROM friend_requests WHERE sender_id=:id")
+	query = text("""
+			  	SELECT a.id, b.username FROM
+					(SELECT receiver_id as id FROM friend_requests WHERE sender_id=:id) a 
+				LEFT JOIN users b ON a.id = b.id
+		""")
+	result = db.session.execute(query, {"id" : id})
+	return result.fetchall()
+
+def get_received_friend_requests(id):
+	query = text("""
+			  	SELECT a.id, b.username FROM
+					(SELECT sender_id as id FROM friend_requests WHERE receiver_id=:id) a 
+				LEFT JOIN users b ON a.id = b.id
+		""")
 	result = db.session.execute(query, {"id" : id})
 	return result.fetchall()
 
 def get_friends(id):
 	query = text("""
-			SELECT user1_id FROM contact_pairs WHERE user2_id=:id 
-			UNION 
-			SELECT user2_id FROM contact_pairs WHERE user1_id=:id 
+			SELECT a.id, b.username FROM 
+				(SELECT user1_id as id FROM contact_pairs WHERE user2_id=:id 
+				UNION 
+				SELECT user2_id as id FROM contact_pairs WHERE user1_id=:id) a
+			LEFT JOIN users b ON a.id = b.id
 		""")
 	result = db.session.execute(query, {"id" : id})
 	return result.fetchall()
@@ -77,3 +92,28 @@ def is_friends_with(id1, id2):
 		""")
 	result = db.session.execute(query, {"id1" : id1, "id2" : id2})
 	return result.fetchone() != None
+
+def get_recently_messaged_with(id):
+	query = text("""
+			SELECT
+				other_id,
+				b.username as other_name,
+				sender_name,
+				date,
+				content
+			FROM (
+				SELECT
+					CASE WHEN sender_id = :id THEN receiver_id ELSE sender_id END AS other_id,
+					sender_id,
+					b.username as sender_name,
+					TO_CHAR(sent_at, 'MM:HH DD/MM/YYYY') AS date,
+					content,
+					ROW_NUMBER() OVER(PARTITION BY LEAST(sender_id, receiver_id), GREATEST(sender_id, receiver_id) ORDER BY a.id DESC) rn
+				FROM messages a
+				LEFT JOIN users b ON a.sender_id = b.id
+			) a
+			LEFT JOIN users b ON a.other_id = b.id
+			WHERE rn = 1
+		""")
+	result = db.session.execute(query, {"id" : id})
+	return result.fetchall()
